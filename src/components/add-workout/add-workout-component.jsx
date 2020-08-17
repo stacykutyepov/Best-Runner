239b60cd3eb4803a57e88bdb12c79ccf0e4678a0
Stanislawa Kutyepov <62475313+codemyjourney@users.noreply.github.com>
2020-08-17 15:56:44 -0400

    create add workout button component

---
+++ src/components/add-workout/add-workout-component.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Fab from "@material-ui/core/Fab";
+import AddIcon from "@material-ui/icons/Add";
+
+const AddWorkout = ({ onClick }) => {
+  return (
+    <Fab variant="extended" color="primary" aria-label="add" onClick={onClick}>
+      <AddIcon />
+      Add Workout
+    </Fab>
+  );
+};
+
+
+export default AddWorkout;
