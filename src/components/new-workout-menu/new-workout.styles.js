447087e0ca54ab5dfcc230d0581b208c3c8bcd2c
Stanislawa Kutyepov <62475313+codemyjourney@users.noreply.github.com>
2020-08-17 15:58:21 -0400

    add styles for new-workout pop-up

---
+++ src/components/new-workout-menu/new-workout.styles.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const FormContainer = styled.div`
+position: fixed;
+top: 20px;
+left: 30px;
+margin: 20px auto;
+box-shadow: 0 2px 8px -3px black;
+border-radius: 20px;
+background-color: white;
+padding: 10px;
+display: flex;
+flex-direction: column;
+justify-content: space-between;
+width: 300px;
+height: 420px;
+`;
+
+export const NavContainer = styled.div`
+display: flex;
+align-content: baseline;
+justify-content: space-between;
+`;
+
+export const Title = styled.span`
+font-weight: 600;
+`
