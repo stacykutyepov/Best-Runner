7a8d04a5aa869c76233e476db6727374b936eafa
Stanislawa Kutyepov <62475313+codemyjourney@users.noreply.github.com>
2020-08-18 12:59:06 -0400

    created main page logic and functionality

--- src/components/main-page/main-page.component.jsx
+++ src/components/main-page/main-page.component.jsx
@@ -1,27 +1,44 @@
 import React from "react";
 import { connect } from "react-redux";
-import { toggleWorkoutWindow } from "../../redux/workouts/workouts.actions";
+import {
+  toggleWorkoutWindow,
+  deleteWorkout,
+} from "../../redux/workouts/workouts.actions";
 
 import AddWorkout from "../add-workout/add-workout-component";
 import NewWorkout from "../new-workout-menu/new-workout.component";
+import WorkoutPreview from "../workout-preview/workout-preview.component";
 
-const MainPage = ({ hidden, toggleWindow}) => {
+const MainPage = ({ hidden, toggleWindow, workoutItems, deleteWorkout }) => {
   return (
     <div>
-      <h1>My Workouts</h1>
-      <AddWorkout onClick={toggleWindow}/>
-      {hidden ? <NewWorkout/> : null}
+      <h1>Welcome!</h1>
+      <AddWorkout onClick={toggleWindow} />
+      {hidden ? <NewWorkout /> : null}
+      <h1>Recent Activity</h1>
+      {
+        workoutItems.length ? workoutItems.map((item) => (
+          <WorkoutPreview
+            key={item.id}
+            item={item}
+            deleteWorkout={deleteWorkout}
+          />
+        ))
+        : <span>You dont have any workouts.</span>
+      }
+     
     </div>
   );
 };
 
 const mapStateToProps = (state) => {
   const { workouts } = state;
-  return { hidden: workouts.hidden };
+  return { hidden: workouts.hidden, workoutItems: workouts.workoutItems };
 };
 
 const mapDispatchToProps = (dispatch) => ({
-    toggleWindow: () => dispatch(toggleWorkoutWindow()),
-  });
+  toggleWindow: () => dispatch(toggleWorkoutWindow()),
+  deleteWorkout: (item) => dispatch(deleteWorkout(item)),
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(MainPage);
