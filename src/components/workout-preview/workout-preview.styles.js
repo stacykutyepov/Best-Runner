a14ed7323ff9bcaafbde418c782020301cc1c460
Stanislawa Kutyepov <62475313+codemyjourney@users.noreply.github.com>
2020-08-18 18:03:21 -0400

    styles update

--- src/components/workout-preview/workout-preview.styles.js
+++ src/components/workout-preview/workout-preview.styles.js
@@ -2,27 +2,33 @@ import styled from "styled-components";
 
 export const PreviewContainer = styled.div`
   display: flex;
-  flex-direction: column;
   margin-bottom: 10px;
   padding: 10px;
   justify-content: space-between;
-  width: 260px;
+  max-width: 290px;
   border-radius: 10px;
   box-shadow: 0 2px 8px -3px black;
+
 `;
 
 export const DataContainer = styled.div`
-  display: flex;
+display: flex;
+flex-direction: column;
   justify-content: space-between;
-  align-items: center;
+  align-items: center; 
 `;
 
 export const ButtonsContainer = styled.div`
   display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
 `;
 
 export const Text = styled.span`
+display: block;
   line-height: 1.5rem;
+  align-self: flex-start;
+
 `;
 
 export const TextItalic = styled.span`
@@ -35,6 +41,7 @@ display: flex;
 flex-direction: column;
 justify-content: space-between;
 width: 60%;
-min-height: 170px;
+min-height: 270px;
+max-height: 300px;
 margin-bottom: 10px;
 `
