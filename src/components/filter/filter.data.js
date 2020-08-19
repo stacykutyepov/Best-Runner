41b98564e7cafdf49de13565dce8543c43b9b115
Stanislawa Kutyepov <62475313+codemyjourney@users.noreply.github.com>
2020-08-19 17:10:25 -0400

    created filter data

---
+++ src/components/filter/filter.data.js
@@ -0,0 +1,40 @@
+const FILTER_DATA = [
+  {
+    title: "No Filters",
+    value: "SHOW_ALL",
+  },
+  {
+    title: "date: Oldest - Newest",
+    value: "OLDEST_NEWEST",
+  },
+  {
+    title: "date: Newest - Oldest",
+    value: "NEWEST_OLDEST",
+  },
+  {
+    title: "km: Max - Min",
+    value: "MAX_MIN",
+  },
+  {
+    title: "km: Min - Max",
+    value: "MIN_MAX",
+  },
+  {
+    title: "Walk",
+    value: "WALK",
+  },
+  {
+    title: "Run",
+    value: "RUN",
+  },
+  {
+    title: "Bike",
+    value: "BIKE",
+  },
+  {
+    title: "Ski",
+    value: "SKI",
+  },
+];
+
+export default FILTER_DATA;
