333ee51f183652d4106ccf6e61ae2d439091106a
Stanislawa Kutyepov <62475313+codemyjourney@users.noreply.github.com>
2020-08-19 17:09:30 -0400

    created workout selectors

--- src/redux/workouts/workouts.selectors.js
+++ src/redux/workouts/workouts.selectors.js
@@ -1,7 +1,41 @@
+import { createSelector } from 'reselect'
 
+const getVisibilityFilter = (state) => state.workouts.visibilityFilter;
 
-// export const filterByDate = (workoutItems) => {
-//     const filteredItems = workoutItems.slice()
-//         .sort((a, b) => new Date(b.date) - new Date(a.date));
-//         return [...filteredItems]
-// }
+const getWorkouts = (state) => state.workouts.workoutItems;
+
+const makeGetVisibleTodos = () => {
+    return createSelector(
+        [getVisibilityFilter, getWorkouts],
+        (visibilityFilter, workouts) => {
+
+            switch (visibilityFilter) {
+                case 'SHOW_ALL':
+                    return workouts
+                        .slice()
+                        .sort((a, b) => new Date(b.date) - new Date(a.date))
+
+                case "NEWEST_OLDEST":
+                    return workouts.slice()
+                        .sort((a, b) => new Date(b.date) - new Date(a.date));
+
+                case "OLDEST_NEWEST":
+                    return workouts.slice()
+                        .sort((a, b) => new Date(a.date) - new Date(b.date));
+
+                case "MAX_MIN":
+                    return workouts.slice()
+                        .sort((a, b) => b.distance - a.distance);
+
+                case "MIN_MAX":
+                    return workouts.slice()
+                        .sort((a, b) => a.distance - b.distance);
+
+                default:
+                    return workouts.filter(item => item.type === visibilityFilter.toLowerCase());
+            }
+        }
+    )
+}
+
+export default makeGetVisibleTodos;
