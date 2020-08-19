7282a1ca1b8732b74dfcfd882742a6fd8af073cf
Stanislawa Kutyepov <62475313+codemyjourney@users.noreply.github.com>
2020-08-19 17:10:18 -0400

    created action dispatch

--- src/components/filter/filter-handler.component.jsx
+++ src/components/filter/filter-handler.component.jsx
@@ -1,34 +1,19 @@
 import React from "react";
 import FilterData from "./filter-basic-form.component";
 import { connect } from "react-redux";
-import {
-  filterByDate,
-  filterByDistance,
-} from "../../redux/workouts/workouts.actions";
+import { filterByDate } from "../../redux/workouts/workouts.actions";
+import FILTER_DATA from "./filter.data";
 
-// TO IMPROVE: Would be better to create filterData Array and store data with filter description inside different objects. Then to use .map inside filterHandler.
-const FilterHandler = ({ filterByDate, filterByDistance }) => {
+const FilterHandler = ({ filterByDate }) => {
   return (
     <div>
-      <FilterData
-        title={"Date"}
-        text={"Display by date:"}
-        values={["oldest-newest", "newest-oldest"]}
-        dispatchAction={filterByDate}
-      />
-      <FilterData
-        title={"Distance"}
-        text={"Display by km:"}
-        values={["max-min", "min-max"]}
-        dispatchAction={filterByDistance}
-      />
+      <FilterData dispatchAction={filterByDate} filterData={FILTER_DATA} />
     </div>
   );
 };
 
 const mapDispatchToProps = (dispatch) => ({
   filterByDate: (date) => dispatch(filterByDate(date)),
-  filterByDistance: (distance) => dispatch(filterByDistance(distance)),
 });
 
 export default connect(null, mapDispatchToProps)(FilterHandler);
