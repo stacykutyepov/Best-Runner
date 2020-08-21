b48a7f03f46e2527b414c0cc6aeec6446134212b
Stanislawa Kutyepov <62475313+codemyjourney@users.noreply.github.com>
2020-08-21 17:50:56 -0400

    add page styles

---
+++ src/components/graph-page/graph-page.styles.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const ButtonContainer = styled.div`
+margin-bottom: 20px;
+`
---
+++ src/components/main-page/main-page.styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const OptionsContainer = styled.div`
+display: flex;
+max-width: 300px;
+align-items: center;
+`
+export const ButtonContainer = styled.div`
+height: 50%;
+width: 100%;
+margin-left: 10px;
+`
