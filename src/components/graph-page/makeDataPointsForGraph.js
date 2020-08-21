3d612ba3ec305fef69ecc62e28f5da83b1609cda
Stanislawa Kutyepov <62475313+codemyjourney@users.noreply.github.com>
2020-08-21 18:40:04 -0400

    create a function for datapoints

---
+++ src/components/graph-page/makeDataPointsForGraph.js
@@ -0,0 +1,26 @@
+var DaysOfTheWeek = {
+    0: "Mon",
+    1: "Tue",
+    2: "Wed",
+    3: "Thu",
+    4: "Fri",
+    5: "Sat",
+    6: "Sun",
+  };
+
+  export const makeDataPointForGraph = (weeksDistanceArray) => {
+    //   const reversedArray = weeksDistanceArray;
+    
+    let dataPointsForGraph = [];
+    for (let i = 0; i < weeksDistanceArray.length; i++) {
+      for (let j = 0; j < weeksDistanceArray[i].length; j++) {
+        let value = DaysOfTheWeek[j];
+  
+        let newObjData = {};
+        newObjData["label"] = value;
+        newObjData["y"] = weeksDistanceArray[i][j];
+        dataPointsForGraph.push(newObjData);
+      }
+    }
+    return dataPointsForGraph;
+  }
