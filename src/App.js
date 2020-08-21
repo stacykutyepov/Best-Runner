20f4116949174ffae863a1719804932897f8e7b2
Stanislawa Kutyepov <62475313+codemyjourney@users.noreply.github.com>
2020-08-21 17:51:06 -0400

    react-router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
+import { Switch, Route } from "react-router-dom";
 import MainPage from './components/main-page/main-page.component';
+import GraphPage from './components/graph-page/graph-container';
 
 function App() {
   return (
     <div className="App">
-      <MainPage/>
+      <Switch>
+        <Route exact path="/" component={MainPage} />
+        <Route exact path="/chart" component={GraphPage} />
+      </Switch>
     </div>
   );
 }
