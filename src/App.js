a294c93ceca5c8b814f0745256390916b1d25a95
Stanislawa Kutyepov <62475313+codemyjourney@users.noreply.github.com>
2020-08-27 17:14:48 -0400

    change routes

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import './App.css';
 import { Switch, Route } from "react-router-dom";
 import MainPage from "./pages/main-page/main-page.component";
 import GraphPage from './pages/graph-page/graph-page.component';
+import IntroPage from './pages/intro-page/intro-page.component';
 
 function App() {
   return (
-    <div className="App">
+    < div className="App" >
       <Switch>
-        <Route exact path="/" component={MainPage} />
+        <Route exact path="/" component={IntroPage} />
+        <Route exact path="/main" component={MainPage} />
         <Route exact path="/chart" component={GraphPage} />
       </Switch>
-    </div>
+    </div >
   );
 }
 
