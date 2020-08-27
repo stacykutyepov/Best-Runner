0c685fa0e539b2b86094da1c005d184312c6adf3
Stanislawa Kutyepov <62475313+codemyjourney@users.noreply.github.com>
2020-08-27 17:15:17 -0400

    create a custom button

---
+++ src/components/custom-button/custom-button.styles.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const CustomButtonContainer = styled.button`
+  cursor: pointer;
+  background-color: lightblue;
+  border: 2px solid lightblue;
+
+  color: black;
+  border: none;
+  min-width: 165px;
+  width: auto;
+  height: 50px;
+  letter-spacing: 0.5px;
+  line-height: 50px;
+  padding: 0 28px 0 28px;
+  font-size: 13px;
+  border-radius: 20px;
+  text-transform: uppercase;
+  font-weight: bolder;
+  display: flex;
+  justify-content: center;
+
+  &:hover {
+    outline: none;
+    background-color: #ffffff;
+text-shadow: 1px 1px 1px lightblue;
+    color: #000000;
+    border: 2px solid lightblue;
+  }
+`;
