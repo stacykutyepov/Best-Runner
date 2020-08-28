36af8f21ebd5963e80c25c5acd17782f43b7c9a8
Stanislawa Kutyepov <62475313+codemyjourney@users.noreply.github.com>
2020-08-28 17:02:51 -0400

    add imageUrl to the data

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,18 +2,22 @@ export const WORKOUT_TYPES = [
   {
     title: "Walk",
     value: "WALK",
+    imageUrl: "https://i.ibb.co/vcNZG6q/walk.png"
   },
   {
     title: "Run",
     value: "RUN",
+    imageUrl: "https://i.ibb.co/2MStD01/run.png"
   },
   {
     title: "Bike",
     value: "BIKE",
+    imageUrl: "https://i.ibb.co/hB4jLcX/bike.png"
   },
   {
     title: "Ski",
     value: "SKI",
+    imageUrl: "https://i.ibb.co/wsFCXvr/ski.png"
   },
 ]
 
